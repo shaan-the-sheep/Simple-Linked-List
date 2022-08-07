a297e821c9c4812d4c7a5c8cd42a9a1968433a0c
Shaan Jassal <shaanjassal101@gmail.com>
2022-08-07 16:00:56 +0100

    Simple linked list with test driven development (TDD)
    Signed-off-by: Shaan Jassal <shaanjassal101@gmail.com>

---
+++ linkedlist.h
@@ -0,0 +1,24 @@
+#ifndef __LINKEDLIST_H
+#define __LINKEDLIST_H
+
+struct element* create(char val); 
+
+int append(char value);
+
+int add(int index, char value); 
+
+int del(int index);
+
+int change(int index, char value);
+
+void deleteList(); 
+
+void printList(); 
+
+struct element { 
+	char val; 
+	struct element* prev; 
+	struct element* next; 
+};  
+
+#endif
---
+++ tests.c
@@ -0,0 +1,49 @@
+/*
+ * tests.c
+ * 
+ * Copyright 2022 Shaan <shaan@shaan-VirtualBox>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#include "CppUTest/TestHarness.h"
+#include "CppUTest/CommandLineTestRunner.h"
+#include <stdio.h>
+#include "linkedlist.h"
+
+TEST_GROUP(FirstTestGroup)
+{
+};
+
+TEST(FirstTestGroup, Create_1)
+{
+   CHECK(create('a') != NULL);
+   
+}
+
+TEST(FirstTestGroup, Create_2)
+{
+	struct element* p = create('a');
+	printf("%c\n", (*p).val);
+	CHECK_EQUAL((*p).val,'a');
+   
+}
+
+int main(int ac, char** av){
+	return CommandLineTestRunner::RunAllTests(ac,av);
+}
